c2a9c73180110d59d2fe26989766f446a05dab69
Vishruth-create <vishruthjp@gmail.com>
2025-08-08 23:50:28 +0530

    Divided code into functions

--- Problem_Statement_0/calc.cpp
+++ Problem_Statement_0/calc.cpp
@@ -36,34 +36,45 @@ double perform_op(double lhv, char ch, double rhv) //As the name of the function
     return 0;
 }
 
+void take_input(vector<double> &nums, vector<char> &ops)
+{
+    double num;
+    char op;
+    while(cin >> num >> op)
+        {
+            nums.push_back(num); //pushing back numbers to nums
+            if(op=='=') break; // If an = is detected then stop taking input
+            ops.push_back(op); //Adding operators to op (= will not be added)
+        }
+
+}
+
+double deal_with_vectors(vector<double> nums,vector<char> ops)
+{
+    int size = nums.size();
+    double result;
+
+    for (int i = 1; i < size; i++)
+    {
+        result = perform_op(nums[nums.size()-2], ops[ops.size()-1], nums[nums.size()-1]); //ONe by one passing values to perform_op function and also removing those values then
+        nums.pop_back();
+        nums.pop_back();
+        nums.push_back(result); // Adding result as the last value for further calculations on it
+        ops.pop_back(); // Removing the operator we are already done with
+    }
+    return result;
+}
+
 int main()
 {
     while (true)
     {
         vector<double> nums = {}; //My idea here to take input, specially for multiple numeriacals and operations
         vector<char> ops = {}; //was to store all numericals in oone vector named nums and operators in vector named ops
-        double num;
-        char op;
 
         cout << ">"; // printing my prompt
-        while(cin >> num >> op)
-        {
-            nums.push_back(num); //Adding numbers to nums
-            if(op=='=') break; // If an = is detected then stop taking input
-            ops.push_back(op); //Adding operators to op (= will not be added)
-        }
-
-        int size = nums.size();
-        double result;
-
-        for (int i = 1; i < size; i++)
-        {
-            result = perform_op(nums[nums.size()-2], ops[ops.size()-1], nums[nums.size()-1]); //ONe by one passing values to perform_op function and also removing those values then
-            nums.pop_back();
-            nums.pop_back();
-            nums.push_back(result); // Adding result as the last value for further calculations on it
-            ops.pop_back(); // Removing the operator we are already done with
-        }
+        take_input(nums, ops);
+        double result = deal_with_vectors(nums, ops);
 
         cout << result << endl; // Adding equals to at the end is necessary
         //Operation are performed from right to left
